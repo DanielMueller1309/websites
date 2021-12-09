74e046a89c9c26a5b490da7340a396c9dcb6e240
DanielMueller1309 <danielmueller-dm@gmx.de>
2021-12-09 14:30:03 +0100

    shell_exec alleine

--- first_php_site/index.php
+++ first_php_site/index.php
@@ -1,43 +1,16 @@
 <!DOCTYPE html>
 <html>
 <body>
+<p style=style="text-align: center;">
 
 <?php
-$output = shell_exec('touch ./test.txt');
-echo "<pre>touch ausgeführt: $output</pre>";
-
-echo "<br>";
-
-$output = shell_exec('ls -lart');
-echo "<pre>ls -lart ausgeführt: $output</pre>";
-
-echo "<br>";
-
-$output = shell_exec('history');
-echo "<pre>history ausgeführt: $output</pre>";
-
-echo "<br>";
-
-$output = shell_exec('pwd');
-echo "<pre>pwd ausgeführt: $output</pre>";
-
-echo "<br>";
-$output = shell_exec('users');
-echo "<pre>users ausgeführt: $output</pre>";
-
-echo "<br>";
-
-$output = shell_exec('groups');
-echo "<pre>groups ausgeführt: $output</pre>";
-
-echo "<br>";
 
 $output = shell_exec('cat index.php');
 echo "<pre>cat ausgeführt: $output</pre>";
 
 echo "<br>";
 ?>
-
+</p>
 
 </body>
 </html>
