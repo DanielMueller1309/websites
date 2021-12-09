32d25ab2cefae2f4e0e06c063a98d8fb0ec1f9eb
DanielMueller1309 <danielmueller-dm@gmx.de>
2021-12-09 14:42:28 +0100

    zeigt nix mehr an

--- first_php_site/index.php
+++ first_php_site/index.php
@@ -1,14 +1,11 @@
 <!DOCTYPE html>
 <html>
 <body>
+
 <p style=style="text-align: center;">
-
 <?php
-
 $output = shell_exec('cat index.php');
 echo "cat ausgeführt: $output";
-
-echo "<br>";
 ?>
 </p>
 
